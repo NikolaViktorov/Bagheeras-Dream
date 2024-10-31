2f8863c7563299cc69ae398fd6fa1c4e5862cd52
Nikola Viktorov <40743601+NikolaViktorov@users.noreply.github.com>
2024-10-31 17:49:34 +0100

    Uploading Website
    Uploading the whole website.

--- Services/BagheerasDream.Services.Data/AdministrationService.cs
+++ Services/BagheerasDream.Services.Data/AdministrationService.cs
@@ -1,167 +1,169 @@
 ﻿namespace BagheerasDream.Services.Data
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Drawing;
+	using System;
+	using System.Collections.Generic;
+	using System.Drawing;
 	using System.IO;
 	using System.Threading.Tasks;
 
-    using BagheerasDream.Data;
+	using BagheerasDream.Data;
 	using BagheerasDream.Data.Models.Cats;
-    using BagheerasDream.Data.Models.Enums;
-    using BagheerasDream.Data.Models.Litter;
-    using BagheerasDream.Services.Data.Contracts;
-    using BagheerasDream.Web.ViewModels.Administration;
-    using BagheerasDream.Web.ViewModels.Litter;
-    using Microsoft.EntityFrameworkCore;
+	using BagheerasDream.Data.Models.Enums;
+	using BagheerasDream.Data.Models.Litter;
+	using BagheerasDream.Services.Data.Contracts;
+	using BagheerasDream.Web.ViewModels.Administration;
+	using BagheerasDream.Web.ViewModels.Litter;
+	using Microsoft.EntityFrameworkCore;
 
-    public class AdministrationService : IAdministrationService
-    {
-        private readonly ApplicationDbContext db;
+	public class AdministrationService : IAdministrationService
+	{
+		private readonly ApplicationDbContext db;
 
-        public AdministrationService(ApplicationDbContext db)
-        {
-            this.db = db;
-        }
+		public AdministrationService(ApplicationDbContext db)
+		{
+			this.db = db;
+		}
 
-        public async Task AddCat(CatInputModel input)
-        {
-            var fileName = ImageUploader.UploadFile(input.File, "cats");
+		public async Task AddCat(CatInputModel input)
+		{
+			var fileName = ImageUploader.UploadFile(input.File, "cats");
 
-            if (input.Name == null || input.File == null)
-            {
-                throw new ArgumentException("Има празни полета!");
-            }
+			if (input.Name == null || input.File == null || input.Birthday == null)
+			{
+				throw new ArgumentException("Има празни полета!");
+			}
 
-            var cat = new Cat()
-            {
-                Name = input.Name,
-                Title = input.Title,
-                Birthday = input.Birthday,
-                Gender = (Gender)input.Gender,
-                Breed = (Breed)input.Breed,
-                Colour = (Colour)input.Colour,
-                ProfileImage = fileName,
-            };
+			var cat = new Cat()
+			{
+				Name = input.Name,
+				Title = input.Title,
+				Birthday = input.Birthday,
+				Gender = (Gender)input.Gender,
+				Breed = (Breed)input.Breed,
+				Colour = (Colour)input.Colour,
+				ProfileImage = fileName,
+			};
 
-            await this.db.Cats.AddAsync(cat);
-            await this.db.SaveChangesAsync();
-        }
+			await this.db.Cats.AddAsync(cat);
+			await this.db.SaveChangesAsync();
+		}
 
-        public async Task AddKittenToLitter(KittenInputModel input)
-        {
-            var litter = await this.db.Litters.FirstOrDefaultAsync(l => l.LitterId == input.LitterId);
-            if (litter == null)
-            {
-                throw new ArgumentException("Не съществува такова котило!");
-            }
+		public async Task AddKittenToLitter(KittenInputModel input)
+		{
+			var litter = await this.db.Litters.FirstOrDefaultAsync(l => l.LitterId == input.LitterId);
+			if (litter == null)
+			{
+				throw new ArgumentException("Не съществува такова котило!");
+			}
 
-            if (input.Name == null || input.ProfileImage == null || input.PedigreeImage == null)
-            {
-                throw new ArgumentException("Има празни полета!");
-            }
+			if (input.Name == null || input.ProfileImage == null || input.PedigreeImage == null)
+			{
+				throw new ArgumentException("Има празни полета!");
+			}
 
-            var kittenImage = ImageUploader.UploadFile(input.ProfileImage, "kittenImages");
-            var pedigreeImage = ImageUploader.UploadFile(input.PedigreeImage, "pedigreeImages");
+			var kittenImage = ImageUploader.UploadFile(input.ProfileImage, "kittenImages");
+			var pedigreeImage = ImageUploader.UploadFile(input.PedigreeImage, "pedigreeImages");
 
-            var kitten = new Kitten()
-            {
-                LitterId = litter.LitterId,
-                Litter = litter,
-                Name = input.Name,
-                Gender = (Gender)input.Gender,
-                PedigreeImage = pedigreeImage,
-                ProfileImage = kittenImage,
-            };
+			var kitten = new Kitten()
+			{
+				LitterId = litter.LitterId,
+				Litter = litter,
+				Name = input.Name,
+				Gender = (Gender)input.Gender,
+				PedigreeImage = pedigreeImage,
+				ProfileImage = kittenImage,
+			};
 
-            litter.Kittens.Add(kitten);
+			litter.Kittens.Add(kitten);
 
-            await this.db.Kittens.AddAsync(kitten);
-            await this.db.SaveChangesAsync();
-        }
+			await this.db.Kittens.AddAsync(kitten);
+			await this.db.SaveChangesAsync();
+		}
 
-        public async Task AddLitter(LitterInputModel input)
-        {
-            if (input.Name == null || input.MotherName == null || input.FatherName == null || input.Colours == null || input.BloodGroup == null)
-            {
-                throw new ArgumentException("Има празни полета!");
-            }
+		public async Task AddLitter(LitterInputModel input)
+		{
+			if (input.Name == null || input.MotherName == null || input.FatherName == null || input.Colours == null || input.BloodGroup == null)
+			{
+				throw new ArgumentException("Има празни полета!");
+			}
 
-            var litter = new Litter()
-            {
-                Name = input.Name,
-                Birthday = input.Birthday,
-                Colours = input.Colours,
-                BloodGroup = input.BloodGroup,
-                FatherName = input.FatherName,
-                MotherName = input.MotherName,
-                Kittens = new List<Kitten>(),
-            };
+			var litter = new Litter()
+			{
+				Name = input.Name,
+				Birthday = input.Birthday,
+				Colours = input.Colours,
+				BloodGroup = input.BloodGroup,
+				FatherName = input.FatherName,
+				MotherName = input.MotherName,
+				Kittens = new List<Kitten>(),
+			};
 
-            await this.db.Litters.AddAsync(litter);
-            await this.db.SaveChangesAsync();
-        }
+			await this.db.Litters.AddAsync(litter);
+			await this.db.SaveChangesAsync();
+		}
 
-        public async Task DeleteCat(string catId)
-        {
-            var cat = await this.db.Cats.FirstOrDefaultAsync(c => c.CatId == catId);
-            if (cat == null)
-            {
-                throw new ArgumentException("There is no cat with given id!");
-            }
 
-            string baseFolderPath = Path.GetFullPath(
-                    Path.Combine(
-                        Directory.GetCurrentDirectory(), @"..\..\")
-                    );
-            string clientFolderPath = baseFolderPath + "Web\\BagheerasDream.Web\\wwwroot\\pictures";
-            string deleteDir = Path.Combine(clientFolderPath, "cats");
-            File.Delete(deleteDir + "\\" + cat.ProfileImage);
+		public async Task DeleteCat(string catId)
+		{
+			var cat = await this.db.Cats.FirstOrDefaultAsync(c => c.CatId == catId);
+			if (cat == null)
+			{
+				throw new ArgumentException("There is no cat with given id!");
+			}
 
-            this.db.Cats.Remove(cat);
-            await this.db.SaveChangesAsync();
-        }
+			string baseFolderPath = Path.GetFullPath(
+					Path.Combine(
+						Directory.GetCurrentDirectory())
+					);
+			string clientFolderPath = baseFolderPath + "/wwwroot/pictures";
+			string deleteDir = Path.Combine(clientFolderPath, "cats");
+			File.Delete(deleteDir + "/" + cat.ProfileImage);
 
-        public async Task RemoveKittenFromLitter(RemoveKittenInputModel input)
-        {
-            var litter = await this.db.Litters.Include(l => l.Kittens)
-                .FirstOrDefaultAsync(l => l.LitterId == input.LitterId);
+			this.db.Cats.Remove(cat);
+			await this.db.SaveChangesAsync();
+		}
 
-            var kitten = await this.db.Kittens.FirstOrDefaultAsync(k => k.KittenId == input.KittenId);
+		public async Task RemoveKittenFromLitter(RemoveKittenInputModel input)
+		{
+			var litter = await this.db.Litters.Include(l => l.Kittens)
+				.FirstOrDefaultAsync(l => l.LitterId == input.LitterId);
 
-            this.DeleteKittenImages(kitten);
+			var kitten = await this.db.Kittens.FirstOrDefaultAsync(k => k.KittenId == input.KittenId);
 
-            litter.Kittens.Remove(kitten);
-            this.db.Kittens.Remove(kitten);
-            await this.db.SaveChangesAsync();
-        }
+			this.DeleteKittenImages(kitten);
 
-        public async Task RemoveLitter(string litterId)
-        {
-            var litter = await this.db.Litters.Include(l => l.Kittens)
-               .FirstOrDefaultAsync(l => l.LitterId == litterId);
+			litter.Kittens.Remove(kitten);
+			this.db.Kittens.Remove(kitten);
+			await this.db.SaveChangesAsync();
+		}
 
-            foreach (var kitten in litter.Kittens)
-            {
-                this.DeleteKittenImages(kitten);
-            }
+		public async Task RemoveLitter(string litterId)
+		{
+			var litter = await this.db.Litters.Include(l => l.Kittens)
+			   .FirstOrDefaultAsync(l => l.LitterId == litterId);
 
-            this.db.Kittens.RemoveRange(litter.Kittens);
-            this.db.Litters.Remove(litter);
-            await this.db.SaveChangesAsync();
-        }
+			foreach (var kitten in litter.Kittens)
+			{
+				this.DeleteKittenImages(kitten);
+			}
 
-        private void DeleteKittenImages(Kitten kitten)
-        {
-            string baseFolderPath = Path.GetFullPath(
-            Path.Combine(
-                Directory.GetCurrentDirectory(), @"..\..\")
+			this.db.Kittens.RemoveRange(litter.Kittens);
+			this.db.Litters.Remove(litter);
+			await this.db.SaveChangesAsync();
+		}
+
+
+		private void DeleteKittenImages(Kitten kitten)
+		{
+			string baseFolderPath = Path.GetFullPath(
+			Path.Combine(
+				Directory.GetCurrentDirectory())
 			);
-            string clientFolderPath = baseFolderPath + "Web\\BagheerasDream.Web\\wwwroot\\pictures";
-            string deletePedDir = Path.Combine(clientFolderPath, "pedigreeImages");
-            string deleteImgDir = Path.Combine(clientFolderPath, "kittenImages");
-            File.Delete(deletePedDir + "\\" + kitten.PedigreeImage);
-            File.Delete(deleteImgDir + "\\" + kitten.ProfileImage);
-        }
-    }
+			string clientFolderPath = baseFolderPath + "/wwwroot/pictures";
+			string deletePedDir = Path.Combine(clientFolderPath, "pedigreeImages");
+			string deleteImgDir = Path.Combine(clientFolderPath, "kittenImages");
+			File.Delete(deletePedDir + "/" + kitten.PedigreeImage);
+			File.Delete(deleteImgDir + "/" + kitten.ProfileImage);
+		}
+	}
 }
--- Services/BagheerasDream.Services.Data/ImageUploader.cs
+++ Services/BagheerasDream.Services.Data/ImageUploader.cs
@@ -1,32 +1,32 @@
 ﻿namespace BagheerasDream.Services.Data
 {
-    using System;
-    using System.IO;
+	using System;
+	using System.IO;
 
-    using Microsoft.AspNetCore.Http;
+	using Microsoft.AspNetCore.Http;
 
-    public static class ImageUploader
-    {
-        public static string UploadFile(IFormFile file, string folderName)
-        {
-            string fileName = null;
-            if (file != null)
-            {
-                string baseFolderPath = Path.GetFullPath(
-                    Path.Combine(
-                        Directory.GetCurrentDirectory(), @"..\..\")
-                    );
-                string clientFolderPath = baseFolderPath + "Web\\BagheerasDream.Web\\wwwroot\\pictures";
-                string uploadDir = Path.Combine(clientFolderPath, folderName);
-                fileName = Guid.NewGuid().ToString() + "-" + file.FileName;
-                string filePath = Path.Combine(uploadDir, fileName);
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    file.CopyTo(fileStream);
-                }
-            }
+	public static class ImageUploader
+	{
+		public static string UploadFile(IFormFile file, string folderName)
+		{
+			string fileName = null;
+			if (file != null)
+			{
+				string baseFolderPath = Path.GetFullPath(
+					Path.Combine(
+						Directory.GetCurrentDirectory(), "@../../")
+					);
+				string clientFolderPath = baseFolderPath + "\\wwwroot\\pictures";
+				string uploadDir = Path.Combine(clientFolderPath, folderName);
+				fileName = Guid.NewGuid().ToString() + "-" + file.FileName;
+				string filePath = Path.Combine(uploadDir, fileName);
+				using (var fileStream = new FileStream(filePath, FileMode.Create))
+				{
+					file.CopyTo(fileStream);
+				}
+			}
 
-            return fileName;
-        }
-    }
+			return fileName;
+		}
+	}
 }
